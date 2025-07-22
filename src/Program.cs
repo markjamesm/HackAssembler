168ec9dccb08b19f7b4f02c4539b9c8c1595e834
Mark-James McDougall <contact@markjames.dev>
2025-07-21 21:41:30 -0400

    Implement check to ensure one commandline argument is passed.

--- src/Program.cs
+++ src/Program.cs
@@ -4,13 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        var currDir = Directory.GetCurrentDirectory();
-        var asmFile = $"{currDir}/{args[0]}";
-        var asmInstructions = File.ReadLines(asmFile);
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Usage: HackAssembler <filename>");
+        }
         
-        Console.WriteLine($"Parsing ASM file: {args[0]}");
-        var filename = args[0].Split('.')[0];
-        var parser = new Parser(filename, asmInstructions);
-        parser.Parse();
+        if (args.Length == 1)
+        {
+            var currDir = Directory.GetCurrentDirectory();
+            var asmFile = $"{currDir}/{args[0]}";
+            var asmInstructions = File.ReadLines(asmFile);
+        
+            Console.WriteLine($"Parsing ASM file: {args[0]}");
+            var filename = args[0].Split('.')[0];
+            var parser = new Parser(filename, asmInstructions);
+            parser.Parse();   
+        }
     }
 }
